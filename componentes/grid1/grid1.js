6a90f15e18b9afe7360f991a8270d77c40af2943
patriky brito <patrikybrito@gmail.com>
2023-01-26 19:23:33 -0300

    pag3

--- componentes/grid1/grid1.js
+++ componentes/grid1/grid1.js
@@ -9,6 +9,15 @@ import {
 } from "react-native";
 import { useFonts } from "expo-font";
 
+const ingredients = [
+
+  {
+    name: "Limao Siciliano",
+    image: require("../grid1/imagens/Limao_Siciliano.png"),
+  },
+
+]
+
 
 const Drinks = ({ navigation }) => {
 const [loaded] = useFonts({
@@ -19,19 +28,28 @@ if (!loaded) {
 }
 return (
   <SafeAreaView style={styles.container}>
-    <View style={styles.rectangle}></View>
+    <View style={styles.rectangle}>
+    <View style={styles.circle}>
+    <View style={styles.circle1}>
+      <TouchableOpacity style={styles.textproduct}>
+        <Text> R$</Text>
+      </TouchableOpacity>
+    </View>
+    </View>
+    </View>
     <View style={styles.logoposition}>
       <Image source={require("../../imagens/logo.png")} />
     </View>
-    <View style={styles.circle}>
-      <TouchableOpacity>
-        <Text style={styles.textproduct}> R$</Text>
-      </TouchableOpacity>
-    </View>
 
     <Text style={styles.textingredients}> Escolha os Igredientes.</Text>
+    <View >
+    <ScrollView horizontal style={styles.ScrollViewtext}>
+    <View style={styles.rectangle2}></View>
 
-    <ScrollView horizontal={true}></ScrollView>
+
+
+    </ScrollView>
+    </View>
   </SafeAreaView>
 );
 }
@@ -40,6 +58,62 @@ export default Drinks;
 export const styles = StyleSheet.create({
 container: {
 flex: 1,
-backgroundColor: "white",
+backgroundColor: "#D6E3E9",
 },
+rectangle:{
+  backgroundColor: "#219EBC",
+  width:"100%",
+  height:"50%",
+  zIndex:0
+},
+logoposition:{
+  alignItems: "flex-start",
+  justifyContent: "center",
+  margin: "5%",
+  position: "absolute",
+},
+circle: {
+  position:"absolute",
+  backgroundColor:"#D6E3E9",
+  height: 160,
+  width: 162,
+  borderRadius: 100,
+  top:"78%",
+  left:"2%",
+},
+  circle1: {
+    position:"absolute",
+    backgroundColor:"white",
+    height: 150,
+    width: 150,
+    borderRadius: 100,
+    left:"3.5%",
+    top:"5%"
+  },
+  textproduct:{
+    position:"absolute",
+    justifyContent:"center",
+    left:"30%",
+    top:"60%"
+  },
+  textingredients:{
+    position:"absolute",
+    top:"62%",
+    margin:"2%",
+    fontSize:16,
+    fontFamily:"EBGaramond"
+  },
+  rectangle2: {
+    backgroundColor:"black",
+    height: 100,
+    width: 70,
+    borderRadius: 30,
+    marginTop:120,
+    left:"1%"
+    
+  },
+  ScrollViewtext:{
+    borderWidth:2,
+    borderColor:"red",
+  }
 });
