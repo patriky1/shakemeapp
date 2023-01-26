e96ba0e97c90680362d1e67fc3625009e281aa56
patriky brito <patrikybrito@gmail.com>
2023-01-25 22:22:37 -0300

    flexwrap

--- App.js
+++ App.js
@@ -14,14 +14,11 @@ import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import cocktails from "./componentes/grid1/grid1.js";
 import Bebidas from "./componentes/grid2/grid2.js";
 
-
-
 // SVG //
 import Img1 from "./imagens/imagensSVG/img1.svg";
 import Inicio from "./imagens/imagensSVG/inicio.svg";
 import Overlay from "./imagens/imagensSVG/overlay.svg";
-import LogoApp from "./componentes/grid2/imagens/logoapp.svg"
-
+import LogoApp from "./componentes/grid2/imagens/logoapp.svg";
 
 // TELA 1 //
 function Home({ navigation }) {
@@ -31,32 +28,37 @@ function Home({ navigation }) {
   if (!loaded) {
     return null;
   }
+  const strings = {
+    titulo:
+      "O importante é ser feliz. O resto, a gente mistura com alguma coisa alcoólica!",
+    botoes: ["Bebidas", "Cocktails"],
+  };
+
   return (
     <SafeAreaView style={styles.container}>
-      <Img1 width={300} height={200} left="12%" top="7%"/>
-      <Inicio width= "100%" height= "100%" bottom="10%"/>
+      <Img1 width={300} height={200} left="12%" top="7%" />
+      <Inicio width="100%" height="100%" bottom="10%" />
       <View style={styles.logoposition}>
         <Image source={require("./imagens/logo.png")} />
       </View>
-      <Text style={styles.titleStyle}>
-        O importante é ser feliz. O resto, a gente mistura com alguma coisa
-        alcoólica!
-      </Text>
-      <Overlay width={300} height={200} left="14%" bottom="60%"/>
+      <Text style={styles.titleStyle}>{strings.titulo}</Text>
+      <View style={styles.overlay}>
+        <View style={styles.overlayImage}>
+          <Overlay width={300} height={50} />
+        </View>
 
-      <View style={styles.buttonContainer1}>
-        <TouchableOpacity onPress={() => navigation.navigate("Bebidas")}>
-          <Text style={styles.buttonText1}>Bebidas</Text>
-        </TouchableOpacity>
+        <View style={styles.buttonContainer}>
+          <TouchableOpacity onPress={() => navigation.navigate("Bebidas")}>
+            <Text style={styles.buttonText}>{strings.botoes[0]}</Text>
+          </TouchableOpacity>
+        </View>
+
+        <View style={styles.buttonContainer}>
+          <TouchableOpacity onPress={() => navigation.navigate("cocktails")}>
+            <Text style={styles.buttonText}>{strings.botoes[1]}</Text>
+          </TouchableOpacity>
+        </View>
       </View>
-
-      <View style={styles.buttonContainer2}>
-        <TouchableOpacity
-          onPress={() => navigation.navigate("cocktails")}>
-          <Text style={styles.buttonText2}>cocktails</Text>
-        </TouchableOpacity>
-      </View>
-
     </SafeAreaView>
   );
 }
@@ -81,10 +83,10 @@ export default function App() {
 
         <Stack.Screen
           name="Bebidas"
-          options={{ 
-            headerTitle: (props) => ( // App Logo
-            <LogoApp  marginHorizontal="20%"/>
-          ),
+          options={{
+            headerTitle: (
+              props // App Logo
+            ) => <LogoApp marginHorizontal="20%" />,
           }}
           component={Bebidas}
         />
@@ -93,42 +95,34 @@ export default function App() {
   );
 }
 
-
-
 export const styles = StyleSheet.create({
-  buttonContainer1: {
-    position:"absolute",
-    borderRadius: 30,
-    width: "4%",
-    height: 40,
-    backgroundColor: "black",
-    bottom: "11.5%",
-    marginHorizontal: "16%",
-    justifyContent: "center",
+  overlay: {
+    position: "absolute",
+    bottom: "20%",
+    display: "flex",
+    flexDirection: "row",
+    width: "100%",
+    justifyContent: "space-evenly",
+  },
+  overlayImage: {
+    position: "absolute",
     alignItems: "center",
+    width: "100%",
+    left: 0,
   },
-  buttonText1: {
-    textTransform: "uppercase",
-    color: "#fff",
-    fontSize: 12,
-  },
-
-  buttonContainer2: {
-    position:"absolute",
+  buttonContainer: {
+    marginTop: "1%",
+    borderRadius: 30,
     width: "25%",
-    borderRadius: 30,
     height: 40,
     backgroundColor: "black",
-    bottom: "11.5%",
-    left:"60%",
     justifyContent: "center",
     alignItems: "center",
   },
-  buttonText2: {
+  buttonText: {
     textTransform: "uppercase",
     color: "#fff",
     fontSize: 12,
-  
   },
 
   container: {
@@ -161,58 +155,9 @@ export const styles = StyleSheet.create({
     position: "absolute",
   },
 
-  // "TELA 2" //
-  container1: {
+  // TELA 3 //
+  container2: {
     flex: 1,
-    backgroundColor: "#D6E3E9",
+    backgroundColor: "white",
   },
-  rectangle: {
-    position: "absolute",
-    width: "100%",
-    height: "50%",
-    backgroundColor: "#219EBC",
-  },
-
-  circle: {
-    position: "absolute",
-    borderRadius: 100,
-    width: "40%",
-    height: "20%",
-    left: 0,
-    top: "40%",
-    backgroundColor: "#fff",
-  },
-
-  textproduct: {
-    position: "absolute",
-    marginTop: "60%",
-    textTransform: "uppercase",
-    left: "30%",
-    fontStyle: "normal",
-    fontSize: 20,
-  },
-
-  textingredients: {
-    fontFamily: "EBGaramond",
-    position: "absolute",
-    top: "60%",
-    padding: 15,
-    fontSize: 20,
-    textAlign: "center",
-    lineHeight: 20,
-  },
-  imgingredients: {
-    height: "15%",
-    left: 0,
-    marginLeft: 10,
-  },
-
-
-
-
-// TELA 3 //
-container2: {
-  flex: 1,
-  backgroundColor: "white",
-},
 });
--- componentes/grid2/grid2.js
+++ componentes/grid2/grid2.js
@@ -1,82 +1,87 @@
-
-import React, {useState} from "react";
-import { StyleSheet, Text, View, Dimensions, FlatList, Image, SafeAreaView, TouchableOpacity } from "react-native";
+import React, { useState } from "react";
+import {
+  StyleSheet,
+  Text,
+  View,
+  Dimensions,
+  ScrollView,
+  Image,
+  SafeAreaView,
+  TouchableOpacity,
+} from "react-native";
 import { useFonts } from "expo-font";
 
 const listTab = [
   {
-    status: 'Todos'
+    status: "Todos",
   },
   {
-    status: 'Vodka'
+    status: "Vodka",
   },
   {
-    status: 'Cerveja'
-  },  
-  {
-    status: 'Wisky'
+    status: "Cerveja",
   },
-]
+  {
+    status: "Wisky",
+  },
+];
 
-const data =[
+const data = [
   {
-    
-    name: 'Vodka Rocks',
-    status: 'Vodka',
-    image: require("../cards/cardimage/Vodka_rocks.png")
-  }, 
-  { 
-    name: 'Vodka Absolut',
-    status: 'Vodka',
-    image: require("../cards/cardimage/Vodka_absolut.png")
+    name: "Vodka Rocks",
+    status: "Vodka",
+    image: require("../cards/cardimage/Vodka_rocks.png"),
   },
   {
-    name: 'Wisky Jack Deniels',
-    status: 'Wisky',
-    image: require("../cards/cardimage/Wisky_jackdeniels.png")
-  },  
-  {
-    name: 'Wisky Red Label',
-    status: 'Wisky',
-    image: require("../cards/cardimage/Wisky_redlabel.png")
-  },  
-  {
-    name: 'Wisky PassPort Scotch',
-    status: 'Wisky',
-    image: require("../cards/cardimage/wisky_passport.png")
+    name: "Vodka Absolut",
+    status: "Vodka",
+    image: require("../cards/cardimage/Vodka_absolut.png"),
   },
   {
-    name: 'Wisky Oldpar',
-    status: 'Wisky',
-    image: require("../cards/cardimage/wisky_oldpar.png")
+    name: "Wisky Jack Deniels",
+    status: "Wisky",
+    image: require("../cards/cardimage/Wisky_jackdeniels.png"),
   },
   {
-    name: 'Wisky Black and White',
-    status: 'Wisky',
-    image: require("../cards/cardimage/Wisky_blackandwhite.png")
+    name: "Wisky Red Label",
+    status: "Wisky",
+    image: require("../cards/cardimage/Wisky_redlabel.png"),
   },
   {
-    name: 'Wisky Ballatines',
-    status: 'Wisky',
-    image: require("../cards/cardimage/wisky_ballantines.png")
+    name: "Wisky PassPort Scotch",
+    status: "Wisky",
+    image: require("../cards/cardimage/wisky_passport.png"),
   },
   {
-    name: 'Voldka Poliakov',
-    status: 'Vodka',
-    image: require("../cards/cardimage/Vodka_poliakov.png")
+    name: "Wisky Oldpar",
+    status: "Wisky",
+    image: require("../cards/cardimage/wisky_oldpar.png"),
   },
   {
-    name: 'cerveja Pilsen',
-    status: 'Cerveja',
-    image: require("../cards/cardimage/Cerveja_pilsen.png")
-  },  
-  
-
-]
+    name: "Wisky Black and White",
+    status: "Wisky",
+    image: require("../cards/cardimage/Wisky_blackandwhite.png"),
+  },
+  {
+    name: "Wisky Ballatines",
+    status: "Wisky",
+    image: require("../cards/cardimage/wisky_ballantines.png"),
+  },
+  {
+    name: "Voldka Poliakov",
+    status: "Vodka",
+    image: require("../cards/cardimage/Vodka_poliakov.png"),
+  },
+  {
+    name: "cerveja Pilsen",
+    status: "Cerveja",
+    image: require("../cards/cardimage/Cerveja_pilsen.png"),
+  },
+];
 
 const Bebidas = () => {
-  const [status, setStatus] = useState('Todos')
-  const [datalist, setDatalist] = useState(data)
+  const [status, setStatus] = useState("Todos");
+  const [datalist, setDatalist] = useState(data);
   const [loaded] = useFonts({
     NerkoOne: require("../../assets/fonts/NerkoOne-Regular.ttf"),
   });
@@ -84,171 +89,146 @@ const Bebidas = () => {
     return null;
   }
 
-
-  const setStatusFilter = status => {
-    if (status !== 'Todos'){
-      setDatalist([...data.filter(e => e.status === status)])
-    } else{
-      setDatalist(data)
+  const setStatusFilter = (status) => {
+    if (status !== "Todos") {
+      setDatalist([...data.filter((e) => e.status === status)]);
+    } else {
+      setDatalist(data);
     }
-    setStatus(status)
-  }
+    setStatus(status);
+  };
 
-
-
-const renderItem = ({item, index}) =>{
-  return(
-
-  <View style={styles.containerposition}>
-    <View key={index} style={styles.itemContainer}>
-      <View style={styles.itemStatus}>
-        <Image
-        style={styles.itemImage}
-        source={item.image}
-        />
-      </View>
-      <View style={styles.itemBody}>
-        <Text style={styles.itemName}> {item.name}</Text>
-      </View>
-    </View>
-  </View>
-  )
-}
-
-  return(
+  return (
     <SafeAreaView style={styles.container}>
       <View>
-          <Text style={styles.titleStyleBebidas}>TAKE A DRINK</Text>
-          <Text style={styles.text2}>Escolha sua bebida</Text>
-        </View>
-
+        <Text style={styles.titleStyleBebidas}>TAKE A DRINK</Text>
+        <Text style={styles.text2}>Escolha sua bebida</Text>
+      </View>
 
       <View style={styles.listTab}>
-        {
-          listTab.map(e => (
-          <TouchableOpacity horizontal={true}
-          style={[styles.btnTab, status === e.status && styles.btnTabActive]}
-          onPress={() => setStatusFilter(e.status)}
+        {listTab.map((e) => (
+          <TouchableOpacity
+            horizontal={true}
+            style={[styles.btnTab, status === e.status && styles.btnTabActive]}
+            onPress={() => setStatusFilter(e.status)}
           >
-            <Text style={[styles.textTab, status === e.status && styles.textTabActive]}>{e.status}</Text>
+            <Text
+              style={[
+                styles.textTab,
+                status === e.status && styles.textTabActive,
+              ]}
+            >
+              {e.status}
+            </Text>
           </TouchableOpacity>
-          ))
-        }
+        ))}
       </View>
-        <FlatList
-        data={datalist}
-        keyExtractor={(e, i) => i.toString()}
-        renderItem={renderItem}
-        />
 
-
-
+      <ScrollView>
+        <View style={styles.list}>
+          {datalist.map((item) => (
+            <View style={styles.containerposition}>
+              <View key={item.name} style={styles.itemContainer}>
+                <View style={styles.itemStatus}>
+                  <Image style={styles.itemImage} source={item.image} />
+                </View>
+                <View style={styles.itemBody}>
+                  <Text style={styles.itemName}> {item.name}</Text>
+                </View>
+              </View>
+            </View>
+          ))}
+        </View>
+      </ScrollView>
     </SafeAreaView>
-
-  )
-}
+  );
+};
 
 export default Bebidas;
 
 const styles = StyleSheet.create({
-  containerposition:{
-    flexDirection:"row",
-    flex:1,
+  list: {
+    display: "flex",
+    flexDirection: "row",
+    flexWrap: "wrap",
+    height: "100%",
+    justifyContent: "space-between",
+  },
+  containerposition: {
+    minHeight: 280,
     width: "45%",
-    height: 280,
-    flexWrap:"wrap",
-    alignItems: "flex-start",
-    justifyContent: "flex-start",
-    
-
   },
-
-  container:{
-    flex:1,
-    paddingHorizontal:10,
-    backgroundColor:"white",
+  itemContainer: {
+    paddingHorizontal: 10,
+    flex: 1,
+    marginVertical: "5%",
+    borderWidth: 1,
+    borderColor: "black",
+    borderRadius: 10,
+    flexWrap: "wrap",
+    flexDirection: "row",
   },
-  listTab:{
-    flexDirection:"row",
-    alignSelf:"center",
-    marginBottom:20,
-    
-
-    
-   
+  container: {
+    flex: 1,
+    paddingHorizontal: 10,
+    backgroundColor: "white",
   },
-  btnTab:{
-    width: Dimensions.get('window').width / 4.5,
-    flexDirection:"row",
+  listTab: {
+    flexDirection: "row",
+    alignSelf: "center",
+    marginBottom: 20,
+  },
+  btnTab: {
+    width: Dimensions.get("window").width / 4.5,
+    flexDirection: "row",
     borderEndWidth: 0.5,
-    borderColor:"white",
-    padding:10,
-    justifyContent:"center",
-    borderRadius:10,
+    borderColor: "white",
+    padding: 10,
+    justifyContent: "center",
+    borderRadius: 10,
   },
-  textTab:{
-    fontSize:16,
+  textTab: {
+    fontSize: 16,
   },
-  btnTabActive:{
-    backgroundColor: "#219EBC"
+  btnTabActive: {
+    backgroundColor: "#219EBC",
   },
-  textTabActive:{
-    color: "#fff"
+  textTabActive: {
+    color: "#fff",
   },
-  itemContainer:{
-    
+
+  itemBody: {
+    paddingVertical: 10,
+    justifyContent: "flex-start",
+    flexDirection: "row",
+    flexWrap: "wrap",
+  },
+
+  itemName: {
+    fontWeight: "bold",
+    fontSize: 12,
+  },
+
+  itemStatus: {
     paddingHorizontal: 10,
-    flex:1,
-    marginVertical: "20%",
-    borderWidth:1,
-    borderColor:"black",
-    borderRadius:10,
-    flexWrap:"wrap",  
-    flexDirection:'row',
-
+    justifyContent: "center",
   },
-
-  itemBody:{
-    paddingVertical:10,
-    justifyContent:"flex-start",
-    flexDirection:"row",
-    flexWrap:"wrap",
-
-    
-  },
-
-  itemName:{
-    fontWeight:"bold",
-    fontSize:12,
-        
-  },
-
-  itemStatus:{
-    paddingHorizontal: 10,
-    justifyContent:"center",
-    
-  },
-  itemImage:{
-    top:"2%",
-    right:"18%",
+  itemImage: {
+    top: "5%",
+    right: "18%",
     width: 200,
     height: 200,
-    
   },
 
   titleStyleBebidas: {
     color: "black",
     fontFamily: "NerkoOne",
-    fontSize: 35,    
+    fontSize: 35,
   },
 
-  
   text2: {
     marginHorizontal: "5%",
     fontSize: 16,
-    paddingVertical:10,
+    paddingVertical: 10,
   },
-
-
-
-})
+});
